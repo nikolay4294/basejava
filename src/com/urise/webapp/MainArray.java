2127a8419adf3731787cad5e25384c0d17e77104
Nikolay4294 <boss1811@yandex.ru>
2021-03-09 19:48:05 +0300

    Lesson03 abstract storage & sorted storage

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -1,8 +1,7 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.SortedArrayStorage;
-import com.urise.webapp.storage.Storage;
+import com.urise.webapp.storage.ArrayStorage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,7 +12,7 @@ import java.io.InputStreamReader;
  * (just run, no need to understand)
  */
 public class MainArray {
-    private final static Storage ARRAY_STORAGE = new SortedArrayStorage();
+    private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,13 +1,9 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.model.Resume;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-    final private Resume[] storage = new Resume[10_000];
-    protected int countResume;
 
     /**
      * @return int, checks the database for a resume and returns the serial number in base.
@@ -15,7 +11,6 @@ public class ArrayStorage extends AbstractArrayStorage {
     @Override
     protected int findIndex(String uuid) {
         for (int i = 0; i < countResume; i++) {
-            System.out.println("файндИндекс " + uuid);
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
