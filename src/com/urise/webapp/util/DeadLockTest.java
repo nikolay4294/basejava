f6320e5e8ec0033239636fa230b17e14b0259666
Nikolay4294 <boss1811@yandex.ru>
2021-08-30 13:56:52 +0300

    HW10 30.08.2021

---
+++ src/com/urise/webapp/util/DeadLockTest.java
@@ -0,0 +1,42 @@
+package com.urise.webapp.util;
+
+public class DeadLockTest {
+    public static final Object Lock1 = new Object();
+    public static final Object Lock2 = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        DeadLockTestDemo1 d1 = new DeadLockTestDemo1();
+        DeadLockTestDemo2 d2 = new DeadLockTestDemo2();
+
+        d1.start();
+        d2.start();
+    }
+
+    private static class DeadLockTestDemo1 extends Thread {
+
+        public void run() {
+            synchronized (Lock1) {
+                System.out.println("Lock1 is holding");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            synchronized (Lock2) {
+                System.out.println("Lock2 is holding");
+            }
+        }
+    }
+
+    private static class DeadLockTestDemo2 extends Thread {
+        public void run() {
+            synchronized (Lock2) {
+                System.out.println("Lock2 is holding");
+            }
+            synchronized (Lock1) {
+                System.out.println("Lock1 is holding");
+            }
+        }
+    }
+}
