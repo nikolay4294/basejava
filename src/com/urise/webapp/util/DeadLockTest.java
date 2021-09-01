27866a46593dd55965b026b2b01530cd2ee09a26
Nikolay4294 <boss1811@yandex.ru>
2021-09-01 23:24:34 +0300

    HW11 01.09.2021

--- src/com/urise/webapp/util/DeadLockTest.java
+++ src/com/urise/webapp/util/DeadLockTest.java
@@ -1,41 +1,51 @@
 package com.urise.webapp.util;
 
-public class DeadLockTest {
-    public static final Object Lock1 = new Object();
-    public static final Object Lock2 = new Object();
+class DeadLockTest {
+    private static final Object Lock1 = new Object();
+    private static final Object Lock2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         executeDeadLock(Lock1, Lock2);
     }
 
     private static void executeDeadLock(Object Lock1, Object Lock2) {
-        Thread thread1 = new Thread(() -> {
-            synchronized (Lock1) {
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        Thread thread0 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("поток " + Thread.currentThread().getName() + " ожидает захвата объекта Lock1");
+                synchronized (Lock1) {
+                    System.out.println("поток " + Thread.currentThread().getName() + " захватил объект Lock1");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("поток " + Thread.currentThread().getName() + " ожидает захвата объекта Lock2");
+                    synchronized (Lock2) {
+                        System.out.println("поток " + Thread.currentThread().getName() + " захватил объект Lock2");
+                    }
                 }
-                System.out.println("Lock1 is holding");
-            }
-            synchronized (Lock2) {
-                System.out.println("Lock2 is holding");
             }
         });
-        Thread thread2 = new Thread(() -> {
-            synchronized (Lock2) {
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("поток " + Thread.currentThread().getName() + " ожидает захвата объекта Lock2");
+                synchronized (Lock2) {
+                    System.out.println("поток " + Thread.currentThread().getName() + " захватил объект Lock2");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("поток " + Thread.currentThread().getName() + " ожидает захвата объекта Lock1");
+                    synchronized (Lock1) {
+                        System.out.println("поток " + Thread.currentThread().getName() + " захватил объект Lock1");
+                    }
                 }
-                System.out.println("Lock2 is holding");
-            }
-            synchronized (Lock1) {
-                System.out.println("Lock1 is holding");
             }
         });
+        thread0.start();
         thread1.start();
-        thread2.start();
     }
 }
