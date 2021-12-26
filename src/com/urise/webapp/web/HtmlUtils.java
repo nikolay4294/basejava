1d05ebca730afffd64db02cdbd93c5a4313cf358
Nikolay4294 <boss1811@yandex.ru>
2021-12-26 17:41:16 +0300

    HW15 26.12.2021

---
+++ src/com/urise/webapp/web/HtmlUtils.java
@@ -0,0 +1,16 @@
+package com.urise.webapp.web;
+
+public class HtmlUtils {
+
+    public String createHtmlHeader(String title){
+        return "<HTML><HEAD><TITLE> " + title + " </TITLE></HEAD><BODY>";
+    }
+
+    public String getTableHead(String align, int border){
+        return "<TABLE align=" + align + " border=" + border + ">";
+    }
+
+    public String getTH(String align, String value){
+        return "<TH align=" + align + "> " + value + " </TH>";
+    }
+}
--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -3,8 +3,10 @@ package com.urise.webapp.web;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
 
-public class ResumeServlet extends HttpServlet {
+public class  ResumeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -18,5 +20,17 @@ public class ResumeServlet extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
         String name = request.getParameter("name");
         response.getWriter().write(name  ==null ? "Hello Resumes!" : "Hello" + name + "!");
+        printTable(request, response);
+    }
+
+    private void printTable(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        HtmlUtils hu = new HtmlUtils();
+
+        out.print(hu.createHtmlHeader("resumes"));
+        out.print(hu.getTableHead("center", 1));
+        out.print(hu.getTH("center", "uuid"));
+        out.print(hu.getTH("center", "fullName"));
     }
 }
