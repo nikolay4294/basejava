0ce8158bdc4cf6c1768497a0f1ed07f5eb489c66
Nikolay4294 <boss1811@yandex.ru>
2022-01-05 00:11:55 +0300

    HW15 05.01.2022

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,15 +1,11 @@
 package com.urise.webapp.web;
 
-import com.urise.webapp.Config;
-import com.urise.webapp.storage.Storage;
-
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -24,13 +20,6 @@ public class ResumeServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
 //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        //Writer writer = response.getWriter();
-        PrintWriter out = response.getWriter();
-        HtmlUtils hu = new HtmlUtils();
-
-        out.print(hu.createHtmlHeader("resumes"));
-        out.print(hu.getTableHead("center", 1));
-        out.print(hu.getTH("center", "uuid"));
-        out.print(hu.getTH("center", "fullName"));
+        Writer writer = response.getWriter();
     }
 }
